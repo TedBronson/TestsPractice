ec6252832f4b00c1581381e467b7b994d20c0ba2
Yaroslav Kabashnyi <kabashnyiyaroslav@gmail.com>
2017-04-06 14:10:51 +0300

    Open a web site

---
+++ PracticeTests/PracticeTests/NUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace PracticeTests
+{
+    class NUnitTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            driver.Url = "http://the-internet.herokuapp.com/";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+
+    }
+}
