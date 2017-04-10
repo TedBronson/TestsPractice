4474bae9644095de180d14066157a7f8eaa63e89
Yaroslav Kabashnyi <kabashnyiyaroslav@gmail.com>
2017-04-10 15:16:12 +0300

    Added status verification for successful request

---
+++ PracticeTests/PracticeTests/APItests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RestSharp;
+
+namespace PracticeTests
+{
+    class APItests
+    {
+        const string BaseUrl = "https://launchlibrary.net/1.1/";
+
+        [Test]
+        public void getListOfLaunches()
+        {
+            var client = new RestClient(BaseUrl);
+
+            //Creating a GET request
+            var request = new RestRequest("launch/Falcon", Method.GET);
+
+            //Executing the request
+            IRestResponse response = client.Execute(request);
+
+            const string statusCode = "OK";
+
+            //HttpStatusCode 
+            Assert.AreEqual(statusCode, response.StatusDescription);
+        }
+
+    }
+}
